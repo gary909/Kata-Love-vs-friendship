7bb347bdcdf298e2155c24598ca7aff44af79d36
gary909 <garywhite61@hotmail.com>
2023-05-25 12:25:48 +0200

    code working

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 function wordsToMarks(string){
-    //your code here
+    // find ascii val, then remove 96 to find num
+    let myArr = [];
+    for(let i = 0; i < string.length; i++){
+        myArr.push(string.charCodeAt(i) - 96);
+    } // then push into array, return summed array:
+    return myArr.reduce((partialSum, a) => partialSum + a, 0);
 }
 
-console.log("attitude"); // 100
-console.log("friends"); // 75
-console.log("family"); // 66
-console.log("selfness"); // 99
-console.log("knowledge"); // 96
+console.log(wordsToMarks("attitude")); // 100
+console.log(wordsToMarks("friends")); // 75
+console.log(wordsToMarks("family")); // 66
+console.log(wordsToMarks("selfness")); // 99
+console.log(wordsToMarks("knowledge")); // 96
