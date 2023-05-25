a74b5ccfc2dd665fd2046137ac6b76c6999e7d8d
gary909 <garywhite61@hotmail.com>
2023-05-25 12:10:10 +0200

    code

---
+++ app.js
@@ -0,0 +1,9 @@
+function wordsToMarks(string){
+    //your code here
+}
+
+console.log("attitude"); // 100
+console.log("friends"); // 75
+console.log("family"); // 66
+console.log("selfness"); // 99
+console.log("knowledge"); // 96
